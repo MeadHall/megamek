887502fee36d3a80600f25734d47fec46cc12b1a
meadhall <mjodet@outlook.com>
2018-06-27 09:52:29 +0200

    Added ability to close advanced search with escape
    Now users can close advanced search with escape button, it will not save currently selected filters.

--- megamek/src/megamek/client/ui/swing/AdvancedSearchDialog.java
+++ megamek/src/megamek/client/ui/swing/AdvancedSearchDialog.java
@@ -27,6 +27,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
@@ -38,19 +39,24 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Vector;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.DefaultCellEditor;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultRowSorter;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
 import javax.swing.RowFilter;
 import javax.swing.RowSorter.SortKey;
@@ -85,6 +91,7 @@ public class AdvancedSearchDialog extends JDialog implements ActionListener,
      *
      */
     private static final long serialVersionUID = 1L;
+    private static final String CLOSE_ACTION = "closeAction";
     /**
      *
      */
@@ -575,6 +582,22 @@ public class AdvancedSearchDialog extends JDialog implements ActionListener,
                 (frame.getLocation().y + (frame.getSize().height / 2)) -
                 (getSize().height / 2));
         setLocation(x, y);
+
+        Action closeAction = new AbstractAction() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                isCanceled = true;
+            }
+        };
+
+        KeyStroke escapeKey = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+        JRootPane rootPane = getRootPane();
+        rootPane.getInputMap().put(escapeKey, CLOSE_ACTION);
+        rootPane.getActionMap().put(CLOSE_ACTION, closeAction);
+        rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKey, CLOSE_ACTION);
+        rootPane.getActionMap().put(CLOSE_ACTION, closeAction);
     }
 
 
